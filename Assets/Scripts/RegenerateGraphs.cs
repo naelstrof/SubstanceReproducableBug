2a568879f5a1a65f16ced2112d7c88c5a361f257
Naelstrof <naelstrof@gmail.com>
2020-05-27 18:58:12 -0600

    first commit

---
+++ Assets/Scripts/RegenerateGraphs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegenerateGraphs : MonoBehaviour {
+    public List<Substance.Game.SubstanceGraph> graphs = new List<Substance.Game.SubstanceGraph>();
+    void Start() {
+        foreach (Substance.Game.SubstanceGraph g in graphs) {
+            g.SetInputVector2Int("$outputsize", 10, 10);
+            g.QueueForRender();
+        }
+        Substance.Game.Substance.RenderSubstancesAsync();
+    }
+}
